6ca3a41ff35a10503a0a1cfd36950ad1133894e1
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-04-22 21:09:05 +0300

    feat[firebase]: Get data from firesore.

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import {
+    getFirestore, collection, getDocs
+} from "firebase/firestore"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -11,4 +14,22 @@ const firebaseConfig = {
     measurementId: "G-0V8P8VVYS1"
 };
 
+// Init firebase app
 initializeApp(firebaseConfig);
+
+// Init services
+const db = getFirestore();
+
+// Collections ref
+const colRef = collection(db, "tests");
+
+// Get collection data
+getDocs(colRef).then((snapshot) => {
+    let tests = [];
+    snapshot.docs.forEach((doc) => {
+        tests.push({ ...doc.data(), id: doc.id })
+    });
+    console.log(tests);
+}).catch(err => {
+    console.log(err.message);
+})
