5bbb57f51af5289e3b2884a6de6138d15ee07540
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2023-08-08 12:50:53 +0300

    feat: Add base config for content layer.

---
+++ contentlayer.config.js
@@ -0,0 +1,41 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+/** @type {import('contentlayer/source-files').ComputedFields} */
+const computedFields = {
+    slug: 
+    {
+      type: "string",
+      resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    },
+    slugAsParams: 
+    {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+    },
+  }
+
+export const Post = defineDocumentType(() => ({
+    name: 'Post',
+    filePathPattern: `blog/**/*.mdx`,
+    fields: 
+    {
+        title: 
+        { 
+            type: 'string', 
+            required: true 
+        },
+        description: 
+        {
+            type: "string",
+            required: true
+        },
+        date: 
+        { 
+            type: 'date', 
+            required: true 
+        },
+    },
+    computedFields
+}))
+
+export default makeSource({ contentDirPath: 'content', documentTypes: [Post] })
