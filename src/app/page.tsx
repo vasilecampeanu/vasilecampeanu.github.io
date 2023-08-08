ca3df91b2da843f8f1daa6897e6d3f7e2dd9f618
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2023-08-08 09:17:53 +0300

    feat: Add new RecentlyPublished.tsx component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import RecentlyPublished from '@/components/RecentlyPublished'
 import { FC } from 'react'
 
 interface HomeProps { }
 
 const Home: FC<HomeProps> = () => {
     return (
-        <>Hello world!</>
+        <RecentlyPublished />
     )
 }
 
---
+++ src/components/RecentlyPublished.tsx
@@ -0,0 +1,14 @@
+interface RecentlyPublishedProps {
+}
+
+const RecentlyPublished: React.FC<RecentlyPublishedProps> = ({}) => {
+    return (
+        <section className="recently-published">
+            <h1 className="section-title">Recently Published</h1>
+            <div className="post-list">
+            </div>
+        </section>
+    )
+}
+
+export default RecentlyPublished;
